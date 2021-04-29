22dc2c793cdd6b98e5caa53bf27a1aa57b83bac7
Jimmy Lin <jimmy.lin0402@gmail.com>
2021-04-29 14:46:10 -0700

    Finished hero text and sidebar. Moving on to Projects page and Bio.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import NavBarOverlay from './components/NavBarOverlay.jsx';
 import Burger from './components/Burger.jsx';
+import { CSSTransition } from 'react-transition-group';
+
 const App = () => {
   return (
-    <div className="homepage_container">
-      <div className="nav_bar">
-        <img className="logo" src="../dist/photos/logo.png"></img>
+    <div className='homepage_container'>
+      <div className='nav_bar'>
+        <img className='logo' src='./photos/logo.png'></img>
         <NavBarOverlay />
+        </div>
+        <div className='hero_text'>
+          <div className='hero_text_divOne'>
+            <h1 className='hero_h1_text'>He<span>ll</span>o, my name is Jimmy<span>L</span>in</h1>
+          </div>
+          <div className='hero_text_divTwo'>
+            <h3 className='first_h3Text'>I'm a Front End Software Engineer</h3>
+          </div>
+          <div className='hero_text_divThree'>
+             <h3 className='second_h3Text'>...with a passion for storyte<span>ll</span>ing.</h3>
+          </div>
         </div>
       </div>
   )
