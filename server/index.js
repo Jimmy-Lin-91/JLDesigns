8bf1558455a28b97102dbe3ae39262467c858da4
Jimmy Lin <jimmy.lin0402@gmail.com>
2021-05-14 14:11:49 -0700

    Nothing important.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const bodyparser = require('body-parser')
 const path = require('path')
 const app = express();
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyparser.json());
 
